7123df4d40c7dc4af2cddc24d4fbdb053ade55a1
Jeff Parrish <jeffhparrish@gmail.com>
2016-02-11 06:58:38 -0800

    Initial fork with patch.sh to shim mongoose.js without native modules.

---
+++ patch.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+set -e
+cp -rf node_modules/mongodb-minimal/bson node_modules
+cp -rf node_modules/mongodb-minimal/es6-promise node_modules
+cp -rf node_modules/mongodb-minimal/mongodb node_modules
+cp -rf node_modules/mongodb-minimal/mongodb-core node_modules
+cp -rf node_modules/mongodb-minimal/nedb node_modules
